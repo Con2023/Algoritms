90f266eaafd32091ef48de02a15d12cf65fd34e8
Sofia K <mekkkkkfa@yandex.ru>
2025-05-24 15:50:55 +0300

    Task1Comment

--- src/CodeRun/BinarySearch/Easy/Task1.java
+++ src/CodeRun/BinarySearch/Easy/Task1.java
@@ -3,49 +3,27 @@ package CodeRun.BinarySearch.Easy;
 import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
-//Реализуйте двоичный поиск в массиве
-//
-//Формат ввода
-//В первой строке входных данных содержатся натуральные числа
-//N
-//N и
-//K
-//K (
-//0
-//        <
-//N
-//        ,
-//K
-//≤
-//        100
-//         
-//        000
-//        0<N,K≤100000). Во второй строке задаются
-//N
-//N элементов первого массива, а в третьей строке –
-//K
-//K элементов второго массива. Элементы обоих массивов - целые числа, каждое из которых по модулю не превосходит
-//1
-//        0
-//        9
-//        10
-//        9
-//
-//
-//Формат вывода
-//Требуется для каждого из K чисел вывести в отдельную строку "YES", если это число встречается в первом массиве, и "NO" в противном случае.
+
+
+/* Задание: Реализуйте двоичный поиск в массиве
+Ввод: В первой строке входных данных содержатся натуральные числа N и K. Во второй строке задаются N элементов первого массива, а в третьей строке – K элементов второго массива.
+Элементы обоих массивов - целые числа.
+Вывод: Требуется для каждого из K чисел вывести в отдельную строку "YES", если это число встречается в первом массиве, и "NO" в противном случае.
+*/
 public class Task1 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        //создаем для ускорения, можно сделать серез классический ввод через System, но это займет больше времени.
 
         String[] firstLine = reader.readLine().split(" ");
         int K = Integer.parseInt(firstLine[1]);
         int N = Integer.parseInt(firstLine[0]);
 
+
         int[] arrN = new int[N];
-        int[] arrK = new int[K];
+        //Читаем строку и задаем массивы по введенному размеру.
 
         System.out.println("Full first array.");
         String[] numsN = reader.readLine().split(" ");
@@ -53,12 +31,15 @@ public class Task1 {
             arrN[i] = Integer.parseInt(numsN[i]);
         }
         Arrays.sort(arrN);
+        //считываем значения из строки, заполняем массив числами и сортируем! это нужно так как мы работаем с бинарным поиском
 
         System.out.println("Full second array.");
         String[] numsK = reader.readLine().split(" ");
         for(int i = 0; i < K; i++){
             int el = Integer.parseInt(numsK[i]);
+            //считываем сторку и заполняем второй массив И сразу выявляем есть ли это число в первом массиве или нет.
             boolean result = findElement(el,arrN);
+            //получаем результат и выводим по условию
             if (result){
                 System.out.println("YES");
             }
@@ -68,25 +49,31 @@ public class Task1 {
         }
 
     }
+    //функция нахождения элемента в другом массиве(можно через contain, но это не интересно)).
+    // Реализуем бинарный поиск через нахождения индекс среднего элемента и сравнения, сокращаем массив поиска.
     public static boolean findElement(Integer el, int[] array){
         int start = 0;
         int end = array.length - 1;
+        // первый и последний индексы
 
         while(start<=end){
+            //пока у нас индекс начала не больше конца выполняется цикл
             int middle = (start+end)/2;
             int guess = array[middle];
-
+            //находим иднекс середины и элемент по этому индексу
             if(guess == el){
                 return true;
             }
+            //если у нас элемент равен загаданномк, то цикл останавливается
             else if(guess > el){
                 end = middle-1;
+                //если средний элемент больше выбранного элемента, то новый индекс конца массива будет равен индекс середины минус 1
             }
             else {
                 start = middle+1;
             }
         }
-
+        //иначе не найдено
         return false;
     }
 }
